1ce5d7e308b9e891f80d08b7db29e872f5314d3c
anmol_sstha <anmolstha777@gmail.com>
2025-11-12 00:04:01 -0500

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+
+ <main>
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <div className="mt-5">About Us and Our Awesome Application</div>
+    </main>
+
+  )
+}
+
+export default AboutPage;
